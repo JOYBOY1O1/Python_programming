14b3278810f31cc7492267e9bb66e1a354bdde9a
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-03 18:10:14 +0530

    Create 18_pr_q9.py

---
+++ Basic/18_pr_q9.py
@@ -0,0 +1,12 @@
+'''
+#list=[]
+tuple=()
+dict={}
+
+Can you change the values inside a list which is contained in set S
+S = {8, 7, 12, “Harry”, [1, 2]}
+
+# List cannot be stored inside a list
+
+Ans: list is unhashable therefore it cannot be changed.
+'''
