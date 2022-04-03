cedba409b8ff892ff666da35e258a9e8c6042add
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-03 18:08:32 +0530

    Create 12_escape_sequence.py

---
+++ Basic/12_escape_sequence.py
@@ -0,0 +1,6 @@
+#Write a program to format the following letter using escape sequence characters.(\n)(\t)
+
+letter ="Dear Harry!, This Pyhton course is nice. Thanks!"
+
+formatted_letter ="\nDear Harry!,\nThis Pyhton course is nice.\n\tThanks!"
+print(formatted_letter)
